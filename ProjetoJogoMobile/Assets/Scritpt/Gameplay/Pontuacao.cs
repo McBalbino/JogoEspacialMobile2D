39cf4e00fffe6ba511dfd2286d07728365816326
Maria Cecylia Balbino <cecyliasb@gmail.com>
2022-01-12 14:56:25 -0300

    adicionando pontuacao

---
+++ ProjetoJogoMobile/Assets/Scritpt/Gameplay/Pontuacao.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pontuacao : MonoBehaviour
+{
+    private int pontos;
+
+    public void Pontuar() 
+    {
+        this.pontos++;
+    }
+}
