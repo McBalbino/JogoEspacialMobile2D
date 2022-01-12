a7474817c11ff97575dbe86d16809b054f31ffd6
Maria Cecylia Balbino <cecyliasb@gmail.com>
2022-01-12 11:39:31 -0300

    injetando dependencias

--- ProjetoJogoMobile/Assets/Scritpt/Gameplay/Seguir.cs
+++ ProjetoJogoMobile/Assets/Scritpt/Gameplay/Seguir.cs
@@ -6,12 +6,20 @@ public class Seguir : MonoBehaviour
 {
     [SerializeField]
     private Transform alvo;
+    [SerializeField]
+    private float velocidade;
 
     // Update is called once per frame
     void Update()
     {
         var deslocamento = alvo.position - transform.position;
         deslocamento = deslocamento.normalized;
+        deslocamento *= this.velocidade;
         this.transform.position += deslocamento * Time.deltaTime;
+    }
+
+    public void SetAlvo(Transform novoAlvo) 
+    {
+        this.alvo = novoAlvo;
     }
 }
