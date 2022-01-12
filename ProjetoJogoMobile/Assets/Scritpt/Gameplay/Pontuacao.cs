e161cc1d9b9e7a3051cc48903ec8c7272ad1e9ea
Maria Cecylia Balbino <cecyliasb@gmail.com>
2022-01-12 15:08:05 -0300

    eventos personalizados

--- ProjetoJogoMobile/Assets/Scritpt/Gameplay/Pontuacao.cs
+++ ProjetoJogoMobile/Assets/Scritpt/Gameplay/Pontuacao.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class Pontuacao : MonoBehaviour
 {
+    [SerializeField]
+    private MeuEventoPersonalizadoInt aoPontuar;
     private int pontos;
 
     public void Pontuar() 
     {
         this.pontos++;
+        this.aoPontuar.Invoke(this.pontos);
     }
+}
+
+[System.Serializable]
+public class MeuEventoPersonalizadoInt : UnityEvent<int>
+{
+
 }
